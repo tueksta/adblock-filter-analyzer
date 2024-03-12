a833ef7aef39d4e03f8bc9b40cce033cfc6835bb
Andreas Beer <>
2024-03-12 13:03:47 +0100

    next try

--- AdBlockSyntaxLine.js
+++ AdBlockSyntaxLine.js
@@ -660,9 +660,9 @@ export class AdBlockSyntaxLine {
 				let s = this.syntax[key];
 				s = this._escapeHTML(s);
 				s = s.replace(/ /g, "&nbsp;");
-				richText += '<span class="' + htmlContent.replace(allSelectorsRegEx, function(match) {
+				richText += '<span class="' + classes + '">' + s.replace(allSelectorsRegEx, function(match) {
 					return '<span class="selectorMarker">' + match + '</span>';
-				}); + '">' + s + '</span>';
+				}); + '</span>';
 			}
 		}
 		return richText;
